f5d79c084ee2bb4d82d3d35cbde8b20cd9961796
EC2 Default User <ec2-user@ip-172-31-7-97.ec2.internal>
2022-03-30 08:26:31 +0000

    rubyメモ課題

---
+++ memo.rb
@@ -0,0 +1,45 @@
+require "csv"
+ 
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+ 
+memo_type = gets.chomp.to_i
+ 
+#続きを書いていきましょう！！(ifで条件分岐)
+
+if memo_type == 1
+      
+  puts "拡張子を除いたファイルを入力してください。"
+  
+  title_name = gets.chomp.to_s
+  
+  puts "メモしたい内容を記入して下さい"
+  puts "完了したらCtrl + Dを押します"
+  
+  memo = STDIN.readline.chomp.to_s
+  
+     CSV.open("#{title_name}.csv","w") do |csv|
+     csv << [memo]
+
+      end
+end
+   
+if memo_type == 2
+  
+  puts "拡張子を除いたファイルを入力してください。"
+  
+  title_name = gets.chomp.to_s
+  
+  puts "編集したい内容を記入して下さい"
+  puts "完了したらCtrl + Dを押します"
+  
+    memo = STDIN.readline.chomp.to_s
+  
+     CSV.open("#{title_name}.csv","a") do |csv|
+     csv << [memo]
+
+      end
+end
+  
+  
+  
+
