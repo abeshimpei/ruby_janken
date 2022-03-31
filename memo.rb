44190fb83516332bffa8c20cf8800bf57a6acb30
EC2 Default User <ec2-user@ip-172-31-7-97.ec2.internal>
2022-03-31 09:35:26 +0000

    メモ課題修正

--- memo.rb
+++ memo.rb
@@ -15,10 +15,10 @@ if memo_type == 1
   puts "メモしたい内容を記入して下さい"
   puts "完了したらCtrl + Dを押します"
   
-   memo = STDIN.readlines.to_s
+   memo = STDIN.readlines
     
      CSV.open("#{title_name}.csv","w") do |csv|
-     csv << [memo]
+     csv << memo
 
       end
 end
@@ -32,10 +32,10 @@ if memo_type == 2
   puts "編集したい内容を記入して下さい"
   puts "完了したらCtrl + Dを押します"
   
-    memo = STDIN.readlines.to_s
+    memo = STDIN.readlines
   
      CSV.open("#{title_name}.csv","a") do |csv|
-     csv << [memo]
+     csv << memo
 
       end
 end
